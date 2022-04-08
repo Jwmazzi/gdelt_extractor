f7588bc3c9ce833dd9ca8edfeefe33140ecdb848
Jeffrey Scarmazzi <jeffreyscarmazzi@gmail.com>
2022-04-07 21:56:42 -0400

    Testing Actions

--- v1.py
+++ v1.py
@@ -4,7 +4,7 @@ import os
 if __name__ == "__main__":
 
     this_dir = os.path.split(os.path.realpath(__file__))[0]
-    config = os.path.join(this_dir, "config.json")
+    config = os.path.join(this_dir, 'config.json')
 
     e = Extractor(config)
 
