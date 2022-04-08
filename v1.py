9ec613b2a3015b2b3d7fddc4406f85e5bafc4339
Lint Action <lint-action@samuelmeuli.com>
2022-04-08 03:01:32 +0000

    Fix code style issues with Black

--- v1.py
+++ v1.py
@@ -4,11 +4,10 @@ import os
 if __name__ == "__main__":
 
     this_dir = os.path.split(os.path.realpath(__file__))[0]
-    config = os.path.join(this_dir, 'config.json')
+    config = os.path.join(this_dir, "config.json")
 
     e = Extractor(config)
 
-    #More Bad Comment
+    # More Bad Comment
 
     e.run_v1()
-    
